90ed5378a562c4966e54f3e065b2690e11c29be3
Guido Mogni <guido.mogni@naranjax.com>
2021-08-22 16:46:14 -0300

    commit 4

--- src/MemoryManager.c
+++ src/MemoryManager.c
@@ -1,13 +1,14 @@
 #include "MemoryManager.h"
 
 typedef struct MemoryManagerCDT {
+
 	char *nextAddress;
+
 } MemoryManagerCDT;
 
 MemoryManagerADT createManager (void *const restrict memoryForManager, void *const restrict managedMemory) {
 
 	MemoryManagerADT memoryManager = (MemoryManagerADT) memoryForManager;
-
 	memoryManager->nextAddress = managedMemory;
 
 	return memoryManager;
@@ -16,7 +17,6 @@ MemoryManagerADT createManager (void *const restrict memoryForManager, void *con
 void *allocMemory (MemoryManagerADT const restrict mem, const size_t memoryToAllocate) {
 
 	char *allocation = mem->nextAddress;
-
 	mem->nextAddress += memoryToAllocate;
 
 	return (void *) allocation;
